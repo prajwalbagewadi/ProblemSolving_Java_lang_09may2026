9bfc7764da8fec8fc9702f116e7915a03834b27d
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2026-05-14 23:37:45 +0530

    JewelsAndStones problem started 14May2026

---
+++ Patterns/Traversal/Easy/Phase2-Counting_And_Tracking/JewelsAndStones.java
@@ -0,0 +1,93 @@
+//Question: https://leetcode.com/problems/jewels-and-stones/description/
+
+/* 
+771. Jewels and Stones
+
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+
+Example 2:
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+ 
+Constraints:
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+*/
+
+class JewelsAndStones {
+    public static void main() {
+
+    }
+}
+
+//Notes
+
+//Array hashing:
+ 
+
+/* 
+int[] freq = new int[256]
+- Why freq size 256, because basic ASCII characters range from 0 to 255
+
+Visual:
+Index Character
+65    A
+97    a
+98    b
+
+Hashing with frequency count:
+- Store how many times each element appears.
+- Instead of searching again and again, you keep counts in hash structure.
+
+Eg:
+String s = "banana"
+count freq:
+b -> 1
+a -> 3
+n -> 2
+
+Example:
+public class Main {
+    public static void main(String[] args) {
+        int val = 'a'; //Storing char into int var gives the chars ascii val.
+        System.out.println("Ascii val of 'a':"+val);
+
+        String s = "Support";
+        int[] freq = new int[256];
+
+        for(char c : s.toCharArray()) {
+            freq[c]++;
+        }
+        System.out.println("Freq count:");
+
+        System.out.println("'s':"+freq['s']);
+        System.out.println("'S':"+freq['S']);
+        System.out.println("'u':"+freq['u']);
+        System.out.println("'p':"+freq['p']);
+        System.out.println("'o':"+freq['o']);
+        System.out.println("'r':"+freq['r']);
+        System.out.println("'t':"+freq['t']);
+    }
+}
+
+/*
+D:\Software_Installed\jdk\bin\java.exe "-javaagent:D:\Software_Installed\IntellJInstall\IntelliJ IDEA Community Edition 2025.2\lib\idea_rt.jar=59368" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\bagew\Desktop\Project_Ideas\problemSolving_java\problemSolving\out\production\problemSolving Main
+Ascii val of 'a':97
+Freq count:
+'s':0
+'S':1
+'u':1
+'p':2
+'o':1
+'r':1
+'t':1
+
+Process finished with exit code 0
+*/
