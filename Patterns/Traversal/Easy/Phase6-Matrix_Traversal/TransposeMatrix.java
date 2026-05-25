f535b5842273a7476b3ad501815435e49c04fa81
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2026-05-25 18:44:25 +0530

    TransposeMatrix problem submitted 25may2026

--- Patterns/Traversal/Easy/Phase6-Matrix_Traversal/TransposeMatrix.java
+++ Patterns/Traversal/Easy/Phase6-Matrix_Traversal/TransposeMatrix.java
@@ -23,12 +23,44 @@ n == matrix[i].length
 -109 <= matrix[i][j] <= 109
 */
 
+/*
+Steps:
+1. Start.
+2. Take input 2d array int[][] matrix.
+3. Create a new 2d array int[][] trans. 
+4. Check if matrix.length!=matrix[0].length.
+5. Set size of trans = new int[matrix[0].length][matrix.length].
+6. For r=0 to matrix.length (increament r++)
+7. For c=0 to matrix[0].length (increment c++)
+8. Set trans[c][r] = matrix[r][c]. //transpose formula
+9. Else Set size trans = new int[matrix.length][matrix[0].length].
+10. For r=0 to matrix.length (increament r++)
+11. For c=0 to matrix[0].length (increment c++)
+12. Set trans[r][c] = matrix[c][r].
+13. Return trans.
+14. Stop.
+*/
+
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        int[][] trans = new int[matrix.length][matrix[0].length];
-        for(int r=0; r<matrix.length; r++) {
-            for(int c=0; c<matrix[r].length; c++) { 
-                trans[r][c] = matrix[c][r];
+        int[][] trans;
+        if(matrix.length!=matrix[0].length) {
+            //System.out.println("n!=m");
+            trans = new int[matrix[0].length][matrix.length];
+            for(int r=0; r<matrix.length; r++) {
+                //System.out.println("r"+r);
+                for(int c=0; c<matrix[0].length; c++) {
+                    //System.out.println("c"+c);
+                    trans[c][r] = matrix[r][c];
+               }
+            }
+        } 
+        else {
+            trans = new int[matrix.length][matrix[0].length];
+            for(int r=0; r<matrix.length; r++) {
+                for(int c=0; c<matrix[r].length; c++) {
+                    trans[r][c] = matrix[c][r];
+               }
             }
         }
         return trans;
@@ -43,9 +75,64 @@ class Main {
         Solution s = new Solution();
         for(int[] r : s.transpose(mat)) {
             for(int c : r) {
-                System.out.print(c);
+                System.out.print(" "+c);
             }
             System.out.println();
         }
     }
 }
+
+/*
+Time Complexity:
+Nested loops -> to build result.
+O(n×m)
+*/
+/*
+notes:
+A[r][c] = AT[c][r];
+*/
+
+/*
+hello world.
+n!=m
+r0
+c0
+c1
+c2
+r1
+c0
+c1
+c2
+ 1 4
+ 2 5
+ 3 6
+
+=== Code Execution Successful ===
+
+Wrong logic
+hello world.
+n!=m
+r0
+c0
+c1
+r1
+c0
+c1
+r2
+c0
+ERROR!
+Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2
+	at Solution.transpose(Main.java:36)
+	at Main.main(Main.java:58)
+
+=== Code Exited With Errors ===
+
+
+hello world.
+ERROR!
+Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2
+	at Solution.transpose(Main.java:33)
+	at Main.main(Main.java:55)
+
+=== Code Exited With Errors ===
+*/
