85d1b8deb868f2bafa45fabc3279e6bfb13f92e0
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2026-06-02 08:06:30 +0530

    transpose problem can't handle 3*2 matrix 2june2026
    Fixed the transpose method to handle non-square matrices correctly and added error handling for edge cases.

--- Patterns/02_MatrixTraversal/Easy/Phase1-Absolute_Beginner_Matrix_Traversal/TransposeMatrix.java
+++ Patterns/02_MatrixTraversal/Easy/Phase1-Absolute_Beginner_Matrix_Traversal/TransposeMatrix.java
@@ -25,12 +25,71 @@ n == matrix[i].length
 
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        
+        int[][] mat;
+        int c;
+        if(matrix.length!=matrix[0].length) {
+             mat = new int[matrix[0].length][matrix.length];
+             c = matrix[0].length;
+             for(int i=0; i<c*matrix.length; i++) {
+                 System.out.println("r="+i/c+" c="+i%c);
+                 mat[i/c][i%c] = matrix[i/c][i%c];
+             }
+        }
+        else {
+            mat = new int[matrix.length][matrix[0].length];
+            c = matrix[0].length;
+            for(int i=0; i<matrix.length*c; i++) {
+                mat[i/c][i%c] = matrix[i%c][i/c];
+            }
+        }
+        return mat;
     }
 }
 
 class Main {
     public static void main(String[] args) {
         System.out.println("Start small. Ship something.");
+        int[][] mat = {{1,2,3},{4,5,6},{7,8,9}};
+        //int[][] mat = {{1,2,3},{4,5,6}};
+        Solution s = new Solution();
+        for(int[] i : s.transpose(mat)) {
+            for(int j : i) {
+                System.out.print(j);
+            }
+            System.out.println();
+        } 
+        
     }
 }
+
+
+/*
+Output:
+
+can't handle edge case 3*2 matrix
+Start small. Ship something.
+147
+258
+369
+
+=== Code Execution Successful ===
+
+Start small. Ship something.
+r=0 c=0
+r=0 c=1
+r=0 c=2
+ERROR!
+Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2
+	at Solution.transpose(Main.java:35)
+	at Main.main(Main.java:54)
+
+=== Code Exited With Errors ===
+
+Start small. Ship something.
+ERROR!
+Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2
+	at Solution.transpose(Main.java:37)
+	at Main.main(Main.java:48)
+
+=== Code Exited With Errors ===
+*/
