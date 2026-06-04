4cf8dd52306b044d41d062820e8a95ef06cad45b
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2026-06-04 20:55:11 +0530

    CountNegatives problem submitted 4june2026

--- Patterns/02_MatrixTraversal/Easy/Phase2-Counting_Tracking_In_Matrices/CountNegative.java
+++ Patterns/02_MatrixTraversal/Easy/Phase2-Counting_Tracking_In_Matrices/CountNegative.java
@@ -24,14 +24,51 @@ n == grid[i].length
 Follow up: Could you find an O(n + m) solution?
 */
 
+/*
+Steps:
+1. Start.
+2. Take input 2D array int[][] grid.
+3. Create new var of type int cnt=0 and c=grid[0].length.
+4. For int i=0 to i<grid.length*c; (increament i++).
+5. Check if grid[i/c(row)][i%c(col)] < 0.
+6. Increament cnt++.
+7. Return cnt.
+8. Stop.
+*/
+
 class Solution {
     public int countNegatives(int[][] grid) {
-       
+       int cnt=0,c=grid[0].length;    
+       for(int i=0; i<grid.length*c; i++) {
+           if(grid[i/c][i%c]<0) {
+               cnt++;
+           }
+       }
+       return cnt;
     }
 }
 
 class Main {
     public static void main(String[] args) {
         System.out.println("Start small. Ship something.");
+        int[][] grid = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+        Solution s = new Solution();
+        System.out.println(s.countNegatives(grid));
     }
 }
+
+/*
+Time Complexity:
+1 loop.
+Visits every element once so row * col
+O(n*m).
+*/
+
+/*
+Output:
+
+Start small. Ship something.
+8
+
+=== Code Execution Successful ===
+*/
