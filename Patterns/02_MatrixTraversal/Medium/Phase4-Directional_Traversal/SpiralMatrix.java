5d22ae882d4699c7547e40213e2e500d25781005
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2026-06-23 07:45:37 +0530

    SpiralMatrix problem inprogress 23june2026
    Fix spiral traversal logic to correctly iterate through all matrix elements.

--- Patterns/02_MatrixTraversal/Medium/Phase4-Directional_Traversal/SpiralMatrix.java
+++ Patterns/02_MatrixTraversal/Medium/Phase4-Directional_Traversal/SpiralMatrix.java
@@ -21,15 +21,47 @@ n == matrix[i].length
 -100 <= matrix[i][j] <= 100
 */
 
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        
+        int n=matrix.length;
+        int m=matrix[0].length;
+        List<Integer> out = new ArrayList<>();
+        for(int r=0; r<m; r++) {
+            out.add(matrix[0][r]);
+        }
+        for(int c=m-1-1; c<n; c++) { //skipping 0 index
+            out.add(matrix[c][m-1]);
+        }
+        for(int r=n-1-1; r>=0; r--) {
+            out.add(matrix[m-1][r]);
+        }
+        for(int c=n-1-1; c>=0+1; c--) {
+            out.add(matrix[c][0]);
+        }
+        return out;
     }
 }
 
 class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
+        int[][] mat = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(s.spiralOrder(mat));
         
     }
 }
+
+/*
+Output:
+
+[1, 2, 3, 6, 9, 8, 7, 4]
+
+=== Code Execution Successful ===
+
+[1, 2, 3]
+
+=== Code Execution Successful ===
+*/
