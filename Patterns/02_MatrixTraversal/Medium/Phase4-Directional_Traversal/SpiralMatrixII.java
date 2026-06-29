cfcc7be1e174abb464a08e4eeb0e47fd3daf430b
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2026-06-29 20:58:28 +0530

    Spiral Matrix 2 problem submitted 29june2026

--- Patterns/02_MatrixTraversal/Medium/Phase4-Directional_Traversal/SpiralMatrixII.java
+++ Patterns/02_MatrixTraversal/Medium/Phase4-Directional_Traversal/SpiralMatrixII.java
@@ -17,6 +17,37 @@ Constraints:
 1 <= n <= 20
 */
 
+/*
+Steps:
+1. Start.
+2. Take input int n.
+3. Create a new 2D array int[][] out = new int[n][n].
+4. Create new int top = 0.
+5. Create new int right = n-1.
+6. Create new int bottom = n-1.
+7. Create new int left = 0.
+8. Create new int cnt = 1.
+9. While top<=bottom && left<=right.
+10. For int i=left to i<=right (increament i++). //top
+11. Insert into out[top][i] = cnt.
+12. Increament cnt++.
+13. For int i=top+1 to i<=bottom (increament i++). //right
+14. Insert into out[i][right] = cnt.
+15. Increament cnt++.
+16. For int i=right-1 to i>=left (decreament i--). //bottom
+17. Insert into out[bottom][i] = cnt.
+18. Increament cnt++.
+19. For int i=bottom-1 to i>=top+1 (decreament i--). //left
+20. Insert into out[i][left] = cnt.
+21. Increament cnt++.
+22. Increament top++.
+23. Decrement right--.
+24. Decrement bottom--.
+25. Increament left++.
+26. Return out.
+27. Stop.
+*/
+
 class Solution {
     public int[][] generateMatrix(int n) {
        int[][] out = new int[n][n];
@@ -69,6 +100,11 @@ class Main {
         }
     }
 }
+
+/*
+Time Complexity:
+O(square(n)).
+*/
 
 /*
 output:
